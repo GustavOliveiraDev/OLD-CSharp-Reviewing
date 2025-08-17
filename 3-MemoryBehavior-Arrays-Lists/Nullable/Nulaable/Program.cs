1ba1273a4684ab2a4fc2b0e119c892d21fdf3a5f
Gustavo Luiz de Oliveira <oliveira.g.dev@gmail.com>
2025-08-17 18:16:14 -0300

    Using memory behaviors

---
+++ 3-MemoryBehavior-Arrays-Lists/Nullable/Nulaable/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Nulaable
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double? valor = null;
+            double? valor2 = 5.0;
+
+            //Return Valor ou o valor atribuido ao tipo dele
+            Console.WriteLine(valor.GetValueOrDefault());
+            Console.WriteLine(valor2.GetValueOrDefault());
+
+            //Tem valor ? 
+            Console.WriteLine(valor.HasValue);
+            Console.WriteLine(valor2.HasValue);
+
+
+            if (valor.HasValue)
+                Console.WriteLine(valor.Value);//Valor
+            
+            if (valor2.HasValue)
+                Console.WriteLine(valor2.Value);
+
+            double? x = null;
+            double y = 10;
+
+            double a = x ?? y;
+            Console.WriteLine(a);
+        }
+    }
+}
